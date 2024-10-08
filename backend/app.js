9d85599990f0de45e6338168f571e090ef9fc27c
Jacques Verpoest <jacques.verpoest@gmail.com>
2024-10-08 14:35:05 +0200

    security

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,4 @@
-/** Inports */
+/** Imports */
 require("dotenv").config() // Load environnement variables from .env file to process.env
 const helmet = require("helmet") 
 const express = require("express")
@@ -7,11 +7,10 @@ const path = require("path")
 const booksRoutes = require("./routes/books")
 const userRoutes = require("./routes/user")
 
-
-// Create an express application
+/** Create an express application */ 
 const app = express()
 
-// MongoDB database connection
+/** MongoDB database connection */
 mongoose.connect(process.env.DB_LINK,
   {
     useNewUrlParser: true,
@@ -20,13 +19,12 @@ mongoose.connect(process.env.DB_LINK,
   .then(() => console.log("MangoDB connection success !"))
   .catch(() => console.log("MangoDB connection failed !"))
 
-// Middleware which intercepts all queries with JSON type and builds req.body (replaces body-parser)
+/** Middleware which intercepts all queries with JSON type and builds req.body (replaces body-parser) */
 app.use(express.json())
 
-app.use(helmet()) /** Protect application by setting various HTTP headers (XSS, Clickjacking,...) */
+app.use(helmet({crossOriginResourcePolicy: false})) /** Protect application by setting various HTTP headers (XSS, Clickjacking,...) but allowing images */
 
-
-// Middleware for CORS headers 
+/** Middleware for CORS headers */
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*") // All origins authorized
   res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization") // Some headers authorized
@@ -34,13 +32,13 @@ app.use((req, res, next) => {
   next() // Go to next middleware
 })
 
-// User routes
+/** User routes */
 app.use("/api/auth", userRoutes)
 
-// Books routes
+/** Books routes */
 app.use("/api/books", booksRoutes)
 
-// Static routes
+/** Static routes */
 app.use("/images", express.static(path.join(__dirname, "images"))) // Set image file local path
 
 module.exports = app;
